cc3f7decd33e80c414fbe2e99579a589a75606ef
JVisser96 <jvisser96@yahoo.com>
2016-11-17 16:28:21 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -112,14 +112,14 @@ var navTester = true;
 			console.log(webTop);
 
 			if(height <= webTop) {
-				$("#body").animate({"background-color" : "#d0d0d0"}, 1000);
+				$("#body").stop().animate({"background-color" : "#d0d0d0"}, 1000);
 				$(".navLines").animate({"background-color" : "#737373"}, 1000);
 				$(".span").stop().animate({"background-color" : "#737373"}, 300);
 				
 			} 
 			
 			if(height >= (webTop - 400)) {
-				$("#body").animate({"background-color" : "#272727"}, 1000);
+				$("#body").stop().animate({"background-color" : "#272727"}, 1000);
 				$(".navLines").animate({"background-color" : "#ffffff"}, 1000);
 				$(".span").stop().animate({"background-color" : "#ffffff"}, 300);
 			}
